acfa39cd6f6de3f3c8cd9590d0c37a567d6bcbda
huangqi <qi_huang1@126.com>
2021-05-10 17:56:54 +0800

    版本1”

--- shiyongxiaozhushou/app.js
+++ shiyongxiaozhushou/app.js
@@ -1,3 +1,4 @@
+var f = require('./f');
 // app.js
 App({
   onLaunch() {
@@ -5,37 +6,8 @@ App({
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
     // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-
-       //获取code
-       console.log("code:"+res.code)
-
-       
-       //发送code
-       wx.request({
-         url: 'https://20210330.keirahq.com/getopenid',
-        //  url: 'http://39.101.133.38/getopenid',
-        //  url: 'http://localhost/getcode',
-         method:"POST",
-         data: {
-             'code': res.code
-         },
-         header: {
-           'content-type': 'application/json' //默认值
-         },
-         success: (res) => {
-           console.log("code发送成功")
-           console.log(res.data)
-           //将其放在缓存中
-           wx.setStorageSync('userid', res.data.userid)
-         }
-       })
-     }
-   })
+    f.login();
  },
  
   globalData: {
